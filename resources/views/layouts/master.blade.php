7b3c641ea6f55a96922b53bdd69af20302918f1e
Maulana Yusuf <94507352+YuNouzen@users.noreply.github.com>
2021-12-24 15:13:03 +0700

    Update Footer #2
    Ubah Anything You want menjadi Aqua Fish

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -167,7 +167,7 @@ desired effect
     <footer class="main-footer">
         <!-- To the right -->
         <div class="pull-right hidden-xs">
-            Anything you want
+            Aqua Fish
         </div>
         <!-- Default to the left -->
         <strong>Copyright &copy; 2021 <a href="#">Company</a>.</strong> All rights reserved.
