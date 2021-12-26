df913032a698c72e8e0b92b8216706161d82e9bb
Maulana Yusuf <94507352+YuNouzen@users.noreply.github.com>
2021-12-26 23:25:16 +0700

    Update Logo
    Before = <span class="logo-lg"><b>Home</b> Aqua Fish</span>
    After = <span class="logo-lg"><b>Aqua Fish</b></span>
    Baris = 76

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -73,7 +73,7 @@ desired effect
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>A</b>F</span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Home</b> Aqua Fish</span>
+            <span class="logo-lg"><b>Aqua Fish</b></span>
         </a>
 
         <!-- Header Navbar -->
